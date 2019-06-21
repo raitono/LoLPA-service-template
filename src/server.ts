d1f439b254d9ebdd40e86af7a6f4ec4047f51474
raitono <bpaintx2@gmail.com>
2019-06-21 10:55:21 -0400

    Add unregister on shutdown

--- src/server.ts
+++ src/server.ts
@@ -7,21 +7,35 @@ const debug: any = require('debug')('service:server');
 const server = app.listen(0);
 const port = (<AddressInfo>server.address()).port;
 
-const register = () => {
-  debug('registering...');
-
-  axios.put(`${process.env.REGISTRY_URL}:${process.env.REGISTRY_PORT}`
-  + `/service/register/${process.env.SERVICE_NAME}/${process.env.npm_package_version}/${port}`)
-    .then((res) => {
-      debug(res.data);
-      debug('registed!');
-    });
+const register = async () => {
+  await axios.put(`${process.env.REGISTRY_URL}:${process.env.REGISTRY_PORT}`
+  + `/service/register/${process.env.SERVICE_NAME}/${process.env.npm_package_version}/${port}`);
 };
 
-register();
+const unregister = async () => {
+  await axios.delete(`${process.env.REGISTRY_URL}:${process.env.REGISTRY_PORT}`
+  + `/service/register/${process.env.SERVICE_NAME}/${process.env.npm_package_version}/${port}`);
+};
 
 const interval = setInterval(register, Number(process.env.REGISTER_INTERVAL) * 1000);
 
+const cleanup = async () => {
+  clearInterval(interval);
+  await unregister();
+};
+
+process.on('uncaughtException', async () => {
+  await cleanup();
+  process.exit(0);
+}).on('SIGINT', async () => {
+  await cleanup();
+  process.exit(0);
+}).on('SIGTERM', async () => {
+  await cleanup();
+  process.exit(0);
+});
+
+register();
 debug(`Service listening on port ${port}...`);
 
 export default server;
